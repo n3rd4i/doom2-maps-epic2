59f4d1a9923b0319d83a42d46bc6942bca99e11d
Andrei H <n3rd4i@gmail.com>
2019-11-24 11:06:54 +0100

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'ftp://ftp.fu-berlin.de/pc/games/idgames/levels/doom2/megawads/epic2.zip'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$(Join-Path $toolsDir wad)"
+  url           = $url
+  checksum      = '678C6DC804687F1BD68F82B739ED75AE3FDE0C867BE4ABBF3311908EE945DE4C'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
